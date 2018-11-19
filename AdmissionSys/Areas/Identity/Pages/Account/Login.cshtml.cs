41bff182539e4d9e9ce81f57ee38c542b114aac5
Ruchit Patel <rpruchitpatel14@gmail.com>
2018-11-19 23:46:57 +0530

    confirm email mail sent again if directly logged in

--- AdmissionSys/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AdmissionSys/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Text.Encodings.Web;
 
 namespace AdmissionSys.Areas.Identity.Pages.Account
 {
@@ -18,11 +20,15 @@ namespace AdmissionSys.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<NuvAdUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
+        private readonly UserManager<NuvAdUser> _userManager;
+        private readonly IEmailSender _emailSender;
 
-        public LoginModel(SignInManager<NuvAdUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<NuvAdUser> signInManager, ILogger<LoginModel> logger, UserManager<NuvAdUser> userManager, IEmailSender emailSender)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
             _logger = logger;
+            _emailSender = emailSender;
         }
 
         [BindProperty]
@@ -72,6 +78,7 @@ namespace AdmissionSys.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
+                NuvAdUser user = await _userManager.FindByEmailAsync(Input.Email);
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
@@ -89,7 +96,29 @@ namespace AdmissionSys.Areas.Identity.Pages.Account
                     _logger.LogWarning("User account locked out.");
                     return RedirectToPage("./Lockout");
                 }
-                else
+                if (!user.EmailConfirmed)
+                {
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { userId = user.Id, code = code },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt. You have not confirmed your Email ID. Please check your email for the latest confirmation email");
+                    return Page();
+                    //return RedirectToPage("./confirmonregister");
+                }
+                if (!user.PhoneNumberConfirmed)
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt. You have not confirmed your Phone Number. After confirming your mail follow the steps shown there.");
+                    return Page();
+                }
+               else
                 {
                     ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                     return Page();
