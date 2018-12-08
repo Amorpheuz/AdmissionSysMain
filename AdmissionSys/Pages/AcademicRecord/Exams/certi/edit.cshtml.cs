7eee30b3da8daa71f5cc5419c45d727584100071
Ruchit Patel <rpruchitpatel14@gmail.com>
2018-12-08 15:35:58 +0530

    edit to every feild in academic record

---
+++ AdmissionSys/Pages/AcademicRecord/Exams/certi/edit.cshtml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdmissionSys.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdmissionSys.Pages.AcademicRecord.Exams.certi
+{
+    public class editModel : PageModel
+    {
+        private readonly AdmissionSys.Models.AdmissionSysContext _context;
+        private readonly UserManager<NuvAdUser> _userManager;
+        private Task<NuvAdUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        public editModel(AdmissionSys.Models.AdmissionSysContext context, UserManager<NuvAdUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public Models.AcademicRecord AcademicRecord { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            NuvAdUser user = await GetCurrentUserAsync();
+            var sturecord = from s in _context.Student select s;
+            sturecord = sturecord.Where(ab => ab.userID.Equals(user.Id));
+
+            AcademicRecord = _context.AcademicRecord.FirstOrDefault(m => m.AcademicRecordID == id && m.StudentID == sturecord.FirstOrDefault().StudentID);
+
+            if (AcademicRecord == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            NuvAdUser user = await GetCurrentUserAsync();
+            var sturecord = from s in _context.Student select s;
+            sturecord = sturecord.Where(ab => ab.userID.Equals(user.Id));
+            int sid = sturecord.FirstOrDefault().StudentID;
+            AcademicRecord.CalcPer = AcademicRecord.CalcPer.Replace("%", "");
+
+            AcademicRecord.StudentID = sid;
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(AcademicRecord).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AcademicRecordExist(AcademicRecord.AcademicRecordID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("../../Index");
+        }
+
+        private bool AcademicRecordExist(int id)
+        {
+            return _context.AcademicRecord.Any(e => e.AcademicRecordID == id);
+        }
+    }
+}
