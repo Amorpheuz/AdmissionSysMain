624feed3c7069c54213d2cee2ad77192a03581c6
Ruchit Patel <rpruchitpatel14@gmail.com>
2018-12-10 06:11:32 +0530

    aded new document view feature for admin
    Conflicts:
    	AdmissionSys/AdmissionSys.csproj

---
+++ AdmissionSys/Pages/Admin/Document/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdmissionSys.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdmissionSys.Pages.Admin.Document
+{
+    [Authorize(Roles = "Admin")]
+    public class IndexModel : PageModel
+    {
+        private readonly AdmissionSys.Models.AdmissionSysContext _context;
+        private readonly UserManager<NuvAdUser> _userManager;
+        private Task<NuvAdUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        public IndexModel(AdmissionSys.Models.AdmissionSysContext context, UserManager<NuvAdUser> userManager)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IList<Models.Documents> Documents { get; set; }
+
+        public async Task OnGetAsync(int? id)
+        {
+            var DocumentsIQ = from a in _context.Documents select a;
+
+            Documents = DocumentsIQ.Where(b => b.StudentID == id).ToList();
+        }
+
+    }
+}
