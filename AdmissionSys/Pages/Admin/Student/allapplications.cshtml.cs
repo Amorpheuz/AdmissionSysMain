17e8d3261aa328914a0498aa20f06303e381e3a3
Ruchit Patel <rpruchitpatel14@gmail.com>
2018-12-10 07:16:47 +0530

    added changes for tracking application status

---
+++ AdmissionSys/Pages/Admin/Student/allapplications.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdmissionSys.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdmissionSys.Pages.Admin.Student
+{
+    public class allapplicationsModel : PageModel
+    {
+        private readonly AdmissionSys.Models.AdmissionSysContext _context;
+        private readonly UserManager<NuvAdUser> _userManager;
+
+        public allapplicationsModel(AdmissionSys.Models.AdmissionSysContext context, UserManager<NuvAdUser> userManager)
+        {
+            _userManager = userManager;
+            _context = context;
+
+        }
+
+        public IList<Models.ApplicationList> ApplicationList { get; set; }
+
+        public async Task OnGetAsync(int? id)
+        {
+            ApplicationList = await _context.ApplicationList
+                .Include(a => a.Programs).Where(c => c.StudentID == id).ToListAsync();
+
+        }
+
+    }
+}
---
+++ AdmissionSys/Pages/Admin/Student/moreoptions.cshtml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdmissionSys.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdmissionSys.Pages.Admin.Student
+{
+    public class moreoptionsModel : PageModel
+    {
+        private readonly AdmissionSys.Models.AdmissionSysContext _context;
+        private readonly UserManager<NuvAdUser> _userManager;
+
+        public moreoptionsModel(AdmissionSys.Models.AdmissionSysContext context, UserManager<NuvAdUser> userManager)
+        {
+            _userManager = userManager;
+            _context = context;
+        
+        }
+
+        [BindProperty]
+        public Models.ApplicationList ApplicationList { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var sturec = _context.Student.Where(c => c.StudentID == id);
+
+            ApplicationList =  _context.ApplicationList.FirstOrDefault(m => m.ApplicationListID==id);
+
+            if (ApplicationList == null)
+            {
+                return NotFound();
+            }
+          
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(ApplicationList).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ApplicationListExists(ApplicationList.ApplicationListID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ApplicationListExists(int id)
+        {
+            return _context.ApplicationList.Any(e => e.ApplicationListID == id);
+        }
+
+    }
+}
